afe66b59424051cdde266a185fa9f9251ce910f7
Brayan Camargo <brayan440@hotmail.com>
2020-02-23 12:17:39 -0500

    First markup

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <article class="bannerBox">
+      <div class="bannerBox__logo">
+        <img src="./assets/logo.png" alt="IntelCost">
+      </div>
+      <div class="bannerBox__image">
+      </div>
+      <div class="bannerBox__search searchBox">
+          <input placeholder="Search Here" v-model="newsSearch" @keyup="changeInput" type="text">
+      </div>
+    </article>
+    <div class="container mt40">
+      <div class="row">
+        <!-- Repeat Here -->
+        <div class="col-md-4">
+          <item-box></item-box>
+        </div>
+      </div>
+    </div>
+
+    <!--  -->
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+import itemBox from "./components/itemBox"
+
+// const api = 'https://pixabay.com/api/?key=13119377-fc7e10c6305a7de49da6ecb25'
+
+export default {
+  name: 'app',
+  components: {
+    itemBox
+  },
+  data() {
+    return {
+      page: 1,
+      list: [],
+      newsSearch: '',
+    };
+  },
+  methods: {
+    changeInput() {
+      console.log('Changed')
+    },
+  },
+
+  // computed:{
+  //   number: function(){
+  //     return parseInt(this.test)
+  //   },
+  // }
+}
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+</style>
---
+++ src/components/itemBox.vue
@@ -0,0 +1,46 @@
+<template>
+  <a target="_blank" href="#" class="itemBox">
+    <div class="itemBox__container">
+      <div class="itemBox__divider itemBox__divider--top">
+        <figure class="itemBox__image">
+          <img src="" alt="default">
+        </figure>
+        <div class="itemBox__credits">
+          100 credits
+        </div>
+      </div>
+      <div class="itemBox__divider itemBox__divider--bottom" style="background-image: url(https://mytablemesa.com/Content/uploads/684a5f05-af87-438e-b32a-82bf7e92e729.png)">
+        <div>
+          <h2 class="itemBox__title">
+            item.author
+          </h2>
+          <p class="itemBox__description">
+            item.title
+          </p>
+        </div>
+        <div class="d-flex justify-content-between">
+          <div>
+            <h2 class="itemBox__title itemBox__price">
+              <!-- $14.15 -->
+              item.objectID
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'item-box',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
